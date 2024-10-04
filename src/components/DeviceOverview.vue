8689d9c46828a1f8c42c1a16625e91b1e03db3f1
世间微斯人 <93172457+HT3301601278@users.noreply.github.com>
2024-10-04 21:09:13 +0800

    更新 DeviceOverview.vue

--- src/components/DeviceOverview.vue
+++ src/components/DeviceOverview.vue
@@ -2,17 +2,10 @@
     <div class="device-overview">
       <h3>设备概览</h3>
       <el-row>
-        <el-col :span="12">
+        <el-col :span="24">
           <el-statistic title="总设备数" :value="deviceCount">
             <template #suffix>
               <el-icon><Monitor /></el-icon>
-            </template>
-          </el-statistic>
-        </el-col>
-        <el-col :span="12">
-          <el-statistic title="在线设备" :value="onlineDeviceCount">
-            <template #suffix>
-              <el-icon><Connection /></el-icon>
             </template>
           </el-statistic>
         </el-col>
@@ -26,7 +19,7 @@
   import { useRouter } from 'vue-router';
   import { useStore } from 'vuex';
   import { ElRow, ElCol, ElStatistic, ElButton, ElIcon } from 'element-plus';
-  import { Monitor, Connection } from '@element-plus/icons-vue';
+  import { Monitor } from '@element-plus/icons-vue';
   
   export default {
     name: 'DeviceOverview',
@@ -36,35 +29,31 @@
       ElStatistic,
       ElButton,
       ElIcon,
-      Monitor,
-      Connection
+      Monitor
     },
     setup() {
       const store = useStore();
       const router = useRouter();
       const deviceCount = ref(0);
-      const onlineDeviceCount = ref(0);
-  
+
       const fetchDeviceData = async () => {
         try {
           await store.dispatch('device/fetchDevices');
           const devices = store.getters['device/getDevices'];
           deviceCount.value = devices.length;
-          onlineDeviceCount.value = devices.filter(device => device.status === 'online').length;
         } catch (error) {
           console.error('获取设备数据失败:', error);
         }
       };
-  
+
       const navigateToDeviceManagement = () => {
         router.push('/device-management');
       };
-  
+
       onMounted(fetchDeviceData);
-  
+
       return {
         deviceCount,
-        onlineDeviceCount,
         navigateToDeviceManagement
       };
     }
